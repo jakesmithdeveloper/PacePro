786ec7a41563423ee7c415627c5bb6cb09ae5293
Jake Smith <33109702+jakesmithdeveloper@users.noreply.github.com>
2022-09-19 16:49:43 -0400

    Initial Commit

---
+++ RunnerTools/RunnerToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerToolsApp.swift
+//  RunnerTools
+//
+//  Created by Jake Smith on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct RunnerToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
