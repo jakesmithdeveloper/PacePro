153a8212893ee3fc06d86be6a8f29596b62704a9
Jake Smith <33109702+jakesmithdeveloper@users.noreply.github.com>
2022-12-12 16:57:03 -0500

    track mode moved to MVVM structure

---
+++ Run Tools/Views/Pacing Calculator Views/TrackMode-ViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  TrackMode-ViewModel.swift
+//  Run Tools
+//
+//  Created by Jake Smith on 12/12/22.
+//
+
+import Foundation
+
+extension TrackModePaceView {
+    class ViewModel: ObservableObject {
+        
+        @Published var distanceString = ""
+        @Published var minutesString = ""
+        @Published var secondsString = ""
+        @Published var outputUnit = "mile"
+        @Published var split = 100
+        
+        var distance: Int {
+            return Int(distanceString) ?? 0
+        }
+        
+        var minutes: Int {
+            return Int(minutesString) ?? 0
+        }
+        
+        var seconds: Int {
+            return Int(secondsString) ?? 0
+        }
+        
+        var paceInSeconds: Int {
+            let minutesAsSeconds = minutes * 60
+            return seconds + minutesAsSeconds
+        }
+        
+        var cantCalculate: Bool {
+            return distance == 0 || paceInSeconds == 0
+        }
+        
+        var resultString: String {
+            let time = timeAtSplit(distance: distance, paceInSeconds: paceInSeconds)
+            if cantCalculate {
+                return "Enter Distance and Pace to calculate split time"
+            } else {
+                return("\(distance) at \(time)")
+            }
+        }
+        
+        func metersToMiles(meters: Int) -> Double {
+            return Double(meters) * 0.000621371
+        }
+        
+        func metersToKm(meters: Int) -> Int {
+            return meters * 1000
+        }
+        
+        func kmPaceToMinPace() -> Double {
+            return Double(paceInSeconds) * 1.60934
+        }
+        
+        func totalSecondsToTime(seconds: Int) -> String {
+            let minutes = Int(seconds) / 60 % 60
+            let seconds = Int(seconds) % 60
+            return ("\(minutes > 0 ? "\(String(minutes)):" : "")\(seconds >= 10 ? String(seconds) : "0\(String(seconds))")")
+        }
+        
+        func timeAtSplit(distance: Int, paceInSeconds: Int) -> String {
+            let mtm = metersToMiles(meters: distance)
+            var secondsResult = 0.0
+            if outputUnit == "mile" {
+                secondsResult = Double(paceInSeconds) * mtm
+            } else {
+                secondsResult = kmPaceToMinPace() * mtm
+            }
+            return totalSecondsToTime(seconds: Int(secondsResult))
+        }
+        
+        
+    }
+}
--- Run Tools/Views/Pacing Calculator Views/TrackModePaceView.swift
+++ Run Tools/Views/Pacing Calculator Views/TrackModePaceView.swift
@@ -9,52 +9,18 @@ import SwiftUI
 
 struct TrackModePaceView: View {
     
-    @State private var distanceString = ""
-    @State private var minutesString = ""
-    @State private var secondsString = ""
-    @State private var outputUnit = "mile"
-    @State private var split = 100
+    @StateObject var vm = ViewModel()
+
     @State private var showingSplits = false
-    
-    var distance: Int {
-        return Int(distanceString) ?? 0
-    }
-    
-    var minutes: Int {
-        return Int(minutesString) ?? 0
-    }
-    
-    var seconds: Int {
-        return Int(secondsString) ?? 0
-    }
-    
-    var paceInSeconds: Int {
-        let minutesAsSeconds = minutes * 60
-        return seconds + minutesAsSeconds
-    }
-    
-    var cantCalculate: Bool {
-        return distance == 0 || paceInSeconds == 0
-    }
-    
-    var resultString: String {
-        let time = timeAtSplit(distance: distance, paceInSeconds: paceInSeconds)
-        if cantCalculate {
-            return "Enter Distance and Pace to calculate split time"
-        } else {
-            return("\(distance) at \(time)")
-        }
-    }
-    
     
     let units = ["mile", "kilometer"]
     
     var body: some View {
         Form {
             Section("Distance") {
-                TrackDistancePresets(distance: $distanceString)
+                TrackDistancePresets(distance: $vm.distanceString)
                 HStack {
-                    TextFieldWithText(userInput: $distanceString, prompt: "Distance:", placeholder: "400m")
+                    TextFieldWithText(userInput: $vm.distanceString, prompt: "Distance:", placeholder: "400m")
                         .keyboardType(.numberPad)
                     Text("/ meters")
                 }
@@ -65,15 +31,15 @@ struct TrackModePaceView: View {
                     Text("Average Pace:")
                         .font(.caption)
                     Group {
-                        TextField("minutes", text: $minutesString)
+                        TextField("minutes", text: $vm.minutesString)
                             .keyboardType(.numberPad)
-                        TextField("seconds", text: $secondsString)
+                        TextField("seconds", text: $vm.secondsString)
                             .keyboardType(.numberPad)
                             
                     }
                     .keyboardType(.numberPad)
                 }
-                Picker("Distance Type", selection: $outputUnit) {
+                Picker("Distance Type", selection: $vm.outputUnit) {
                     ForEach(units, id: \.self) {
                         Text("\($0)s")
                     }
@@ -82,13 +48,13 @@ struct TrackModePaceView: View {
             }
             
             Section("Result") {
-                Text("\(resultString)")
-                if cantCalculate == false {
+                Text("\(vm.resultString)")
+                if vm.cantCalculate == false {
                     if showingSplits {
-                        Stepper("splits at every \(split) meters", value: $split, step: 100)
+                        Stepper("splits at every \(vm.split) meters", value: $vm.split, step: 100)
                         List {
-                            ForEach(Array(stride(from: split, through: distance, by: split)), id: \.self) { split in
-                                Text("\(String(split)) : \(timeAtSplit(distance: split, paceInSeconds: paceInSeconds))")
+                            ForEach(Array(stride(from: vm.split, through: vm.distance, by: vm.split)), id: \.self) { split in
+                                Text("\(String(split)) : \(vm.timeAtSplit(distance: split, paceInSeconds: vm.paceInSeconds))")
                             }
                         }
                     }
@@ -103,37 +69,6 @@ struct TrackModePaceView: View {
         .scrollDismissesKeyboard(.immediately)
         .navigationTitle("Track Calculator")
     }
-    
-    
-    func metersToMiles(meters: Int) -> Double {
-        return Double(meters) * 0.000621371
-    }
-    
-    func metersToKm(meters: Int) -> Int {
-        return meters * 1000
-    }
-    
-    func kmPaceToMinPace() -> Double {
-        return Double(paceInSeconds) * 1.60934
-    }
-    
-    func totalSecondsToTime(seconds: Int) -> String {
-        let minutes = Int(seconds) / 60 % 60
-        let seconds = Int(seconds) % 60
-        return ("\(minutes > 0 ? "\(String(minutes)):" : "")\(seconds >= 10 ? String(seconds) : "0\(String(seconds))")")
-    }
-    
-    func timeAtSplit(distance: Int, paceInSeconds: Int) -> String {
-        let mtm = metersToMiles(meters: distance)
-        var secondsResult = 0.0
-        if outputUnit == "mile" {
-            secondsResult = Double(paceInSeconds) * mtm
-        } else {
-            secondsResult = kmPaceToMinPace() * mtm
-        }
-        return totalSecondsToTime(seconds: Int(secondsResult))
-    }
-    
 }
 
 struct TrackModePaceView_Previews: PreviewProvider {
